79dcb053528a509536a27430e404a1f712fe1742
Jonathan da Silva Santiago <jonathan.santiago@live.com>
2019-06-11 14:35:01 -0400

    adicionando nuxtjs

--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,72 @@
 <template>
-  <section class="container">
     <div>
-      <logo/>
-      <h1 class="title">
-        cryptomarket
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <nuxt-link class="button" :to="{name: 'chat'}">CHAT</nuxt-link>
-        <ul class="users">
-          <li v-for="(user, index) in users" :key="index" class="user">
-            <nuxt-link :to="{ name: 'user-id', params: { id: index }}">
-              {{ user.name }}
-            </nuxt-link>
-          </li>
-        </ul>
-      </div>
+        <v-toolbar
+                color="blue darken-2"
+                dark
+                fixed
+                app
+                clipped-right
+        >
+            <v-toolbar-title>HYDRO CONTROL</v-toolbar-title>
+        </v-toolbar>
+        <v-content>
+            <v-layout>
+                <v-flex xs6 lg3>
+                    <v-card class="elevation-7" height="90vh">
+                        <v-card-title primary-title>
+                            <div class="headline">
+                                <!--                                <div class="headline">VAZÃO</div>-->
+                                <span>150 M/S</span> / <span>R$ 150.00</span>
+
+                            </div>
+                        </v-card-title>
+
+                        <v-card-title primary-title>
+                            <div>
+                            </div>
+                        </v-card-title>
+                    </v-card>
+                </v-flex>
+                <v-flex xs6 lg9>
+
+                </v-flex>
+            </v-layout>
+        </v-content>
+        <v-footer color="blue darken-2" class="white--text" app>
+            <span>INOVATECH</span>
+            <v-spacer></v-spacer>
+            <span>&copy; 2019</span>
+        </v-footer>
     </div>
-  </section>
 </template>
 
 <script>
     import axios from '~/plugins/axios'
-    import Logo from '~/components/Logo.vue'
+    import socket from '~/plugins/socket.io.js'
 
     export default {
-        components: {
-            Logo
-        },
-        async asyncData () {
-            let { data } = await axios.get('/api/users')
-            return { users: data }
-        },
-        head () {
+        data() {
             return {
-                title: 'Users'
+                messages: []
+            }
+        },
+        watch: {
+            messages(val) {
+                console.log(val);
+            }
+        },
+        beforeMount() {
+            socket.on('teste', (message) => {
+                console.log(message);
+            })
+        },
+        async asyncData() {
+            let {data} = await axios.get('/api/users')
+            return {users: data}
+        },
+        head() {
+            return {
+                title: 'Hydro Control'
             }
         }
     }
@@ -44,46 +74,46 @@
 
 <style scoped>
 
-  .title {
-    font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-    display: block;
-    font-weight: 300;
-    font-size: 100px;
-    color: #35495e;
-    letter-spacing: 1px;
-  }
+    .title {
+        font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+        display: block;
+        font-weight: 300;
+        font-size: 100px;
+        color: #35495e;
+        letter-spacing: 1px;
+    }
 
-  .subtitle {
-    font-weight: 300;
-    font-size: 42px;
-    color: #526488;
-    word-spacing: 5px;
-    padding-bottom: 15px;
-  }
+    .subtitle {
+        font-weight: 300;
+        font-size: 42px;
+        color: #526488;
+        word-spacing: 5px;
+        padding-bottom: 15px;
+    }
 
-  .users {
-    list-style: none;
-    margin: 0;
-    padding: 0;
-  }
+    .users {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+    }
 
-  .user {
-    margin: 10px 0;
-  }
+    .user {
+        margin: 10px 0;
+    }
 
-  .button, .button:visited {
-    display: inline-block;
-    color: black;
-    letter-spacing: 1px;
-    background-color: #fff;
-    border: 2px solid #000;
-    text-decoration: none;
-    text-transform: uppercase;
-    padding: 15px 45px;
-  }
+    .button, .button:visited {
+        display: inline-block;
+        color: black;
+        letter-spacing: 1px;
+        background-color: #fff;
+        border: 2px solid #000;
+        text-decoration: none;
+        text-transform: uppercase;
+        padding: 15px 45px;
+    }
 
-  .button:hover, .button:focus {
-    color: #fff;
-    background-color: #000;
-  }
+    .button:hover, .button:focus {
+        color: #fff;
+        background-color: #000;
+    }
 </style>
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const { Nuxt, Builder } = require('nuxt')
 const app = require('express')()
-const server = require('http').createServer(app)
+const http = require('http');
+const server = http.createServer(app);
 const io = require('socket.io')(server)
 const port = process.env.PORT || 3000
 const isProd = process.env.NODE_ENV === 'production'
@@ -10,30 +11,42 @@ const api = require('./api');
 // Import API Routes
 app.use('/api', api);
 
-// We instantiate Nuxt.js with the options
 var config = require('../nuxt.config.js')
 config.dev = !isProd
 
 const nuxt = new Nuxt(config)
-// Start build process in dev mode
 if (config.dev) {
   const builder = new Builder(nuxt)
   builder.build()
 }
-app.use(nuxt.render)
 
-// Listen the server
-server.listen(port, '0.0.0.0')
-console.log('Server listening on localhost:' + port) // eslint-disable-line no-console
+app.use(nuxt.render);
+
+server.listen(port, '127.0.0.1')
+console.log('Server listening on localhost:' + port);
 
 // Socket.io
-var messages = []
+const options = {
+  host: 'www.google.com',
+  port: 80,
+  path: '/upload',
+  method: 'POST'
+};
+
+const req = http.request(options, function(res) {
+  console.log('STATUS: ' + res.statusCode);
+  res.setEncoding('utf8');
+  res.on('data', function (chunk) {
+    console.log('BODY: ' + chunk);
+  });
+});
+
+req.on('error', function(e) {
+  console.log('problem with request: ' + e.message);
+});
+
 io.on('connection', (socket) => {
-  socket.on('last-messages', function (fn) {
-    fn(messages.slice(-50))
-  })
-  socket.on('send-message', function (message) {
-    messages.push(message)
-    socket.broadcast.emit('new-message', message)
-  })
-})
+  setInterval(() => {
+    socket.broadcast.emit('teste', Math.random())
+  }, 1000)
+});
