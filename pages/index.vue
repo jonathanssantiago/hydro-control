9932fed5da9a42270ad604d6d7b119e4f12d100b
Jonathan da Silva Santiago <jonathan.santiago@live.com>
2019-06-12 22:26:34 -0400

    adicionando  dados dinamicos

--- pages/index.vue
+++ pages/index.vue
@@ -1,53 +1,73 @@
 <template>
     <div>
         <v-toolbar
-                color="indigo darken-4"
+                color="blue lighten-1"
                 dark
                 fixed
                 app
                 clipped-right
         >
-            <v-toolbar-title>HYDRO CONTROL</v-toolbar-title>
+            <v-toolbar-title>
+                Hydro Control
+            </v-toolbar-title>
         </v-toolbar>
         <v-content>
             <v-layout row wrap>
-                <v-flex xs12 md4 lg3>
-                    <v-card class="elevation-7" height="90vh">
+                <v-flex xs12 md5 lg4>
+                    <v-card class="elevation-7" height="100vh" style="background-color:#BBDEFB">
                         <v-card-text primary-title>
                             <div class="text-xs-center">
-                                <span style="cursor: pointer;"><v-icon x-large left>keyboard_arrow_left</v-icon></span>
-                                <span><b style="font-size: 20pt;">DIÁRIO</b></span>
-                                <span style="cursor: pointer;"><v-icon x-large
+                                <span style="cursor: pointer;" @click="prev"><v-icon x-large left>keyboard_arrow_left</v-icon></span>
+                                <span><b style="font-size: 20pt;" v-if="tipo === 1">DIÁRIO</b></span>
+                                <span><b style="font-size: 20pt;" v-if="tipo === 2">SEMANAL</b></span>
+                                <span><b style="font-size: 20pt;" v-if="tipo === 3">MENSAL</b></span>
+                                <span style="cursor: pointer;" @click="next"><v-icon x-large
                                                                        right>keyboard_arrow_right</v-icon></span>
                             </div>
                         </v-card-text>
-                        <v-divider/>
+                        <v-divider/>    
                         <v-card-text primary-title>
-                            <h1 class="text-xs-center" style="font-size: 50pt">{{vazao}} m3</h1>
+                            <span v-if="tipo === 1">
+                                <h1 class="text-xs-center" style="font-size: 50pt">{{atual.vazao}} m3</h1>
+                            </span>
+                            <span v-if="tipo === 2">
+                                <h1 class="text-xs-center" style="font-size: 50pt">{{semanal.vazao}} m3</h1>
+                            </span>
+                            <span v-if="tipo === 3">
+                                <h1 class="text-xs-center" style="font-size: 50pt">{{mensal.vazao}} m3</h1>
+                            </span>
                         </v-card-text>
                         <v-divider/>
                         <v-card-text primary-title>
-                            <h1 class="text-xs-center" style="font-size: 50pt">R$ {{preco}}</h1>
+                            <span v-if="tipo === 1">
+                                <h1 class="text-xs-center" style="font-size: 50pt">R$ {{atual.preco}}</h1>
+                            </span>
+                             <span v-if="tipo === 2">
+                                <h1 class="text-xs-center" style="font-size: 50pt">R$ {{semanal.preco}}</h1>
+                            </span>
+                             <span v-if="tipo === 3">
+                                <h1 class="text-xs-center" style="font-size: 50pt">R$ {{mensal.preco}}</h1>
+                            </span>
                         </v-card-text>
                         <v-divider/>
+                        <v-card-text>
+                             <div class="text-xs-center">
+                            <img src="~/assets/logo.gif" alt="" width="250" height="250" style="padding: 1rem">
+                     </div>
+                        </v-card-text>
                     </v-card>
                 </v-flex>
-                <v-flex xs12 md7 lg7 offset-lg1 offset-md1>
+                <v-flex xs12 md6 lg6 offset-lg1 offset-md1>
                     <v-card class="elevation-7 mt-2" height="30vh">
                         <v-card-title primary-title>
                             <h1>Avisos</h1>
                         </v-card-title>
 
                         <v-card-text primary-title>
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
-                                has been the industry's standard
-                                dummy text ever since the 1500s, when an unknown printer took a galley of type and
-                                scrambled it to make a type specimen book.
-                                It has survived not only five centuries, but also the leap into electronic typesetting,
-                                remaining essentially unchanged. It was popularised
-                                in the 1960s with the release of
-                                Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
-                                publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                            <p>
+                            <v-icon>warning</v-icon>   Problemas nos encanamentos, vazamentos pequenos em canos
+                               podem desperdiçar mais de 5000 litros de água a longo prazo.
+                            </p>
                         </v-card-text>
 
                         <v-card-title primary-title>
@@ -56,22 +76,19 @@
                         </v-card-title>
                     </v-card>
 
-                    <v-card class="elevation-7 mt-3" height="56vh">
+                    <!-- <v-card class="elevation-7 mt-3" height="53vh">
                         <v-card-title primary-title>
                             <div class="text-xs-center">
                                 <h1>Gráfico</h1>
-                            </div>
-                        </v-card-title>
 
-                    </v-card>
+
+                                dasdas
+                            </div>
+                        </v-card-title> 
+                    </v-card>-->
                 </v-flex>
             </v-layout>
         </v-content>
-        <v-footer color="indigo darken-4" class="white--text" app>
-            <span class="pl-4">INOVATECH</span>
-            <v-spacer></v-spacer>
-            <span class="pr-4">&copy; Hydro Control 2019</span>
-        </v-footer>
     </div>
 </template>
 
@@ -82,19 +99,52 @@
     export default {
         data() {
             return {
-                preco: 0,
-                vazao: 0
+                tipo: 1,
+                atual: {                        
+                    preco: 0,
+                    vazao: 0
+                },
+                 semanal: {
+                    preco: 0,
+                    vazao: 0
+                },
+                 mensal: {
+                    preco: 0,
+                    vazao: 0
+                }
             }
         },
         watch: {
             messages(val) {
-                console.log(val);
+                console.log(val);                                                                                                                                                                                                                                                                       
+            }
+        },
+        methods:{
+            next(){
+                if(this.tipo < 3){
+                    this.tipo++;
+                }
+            },
+             prev(){
+                if(this.tipo > 1){
+                    this.tipo--;
+                }
             }
         },
         beforeMount() {
-            socket.on('teste', (data) => {
-                this.preco = data.preco;
-                this.vazao = data.vazao;
+            socket.on('atual', (data) => {
+                this.atual.preco = data.preco;
+                this.atual.vazao = data.vazao;
+            })
+
+            socket.on('semanal', (data) => {
+                this.semanal.preco = data.preco;
+                this.semanal.vazao = data.vazao;
+            })
+
+            socket.on('mensal', (data) => {
+                this.mensal.preco = data.preco;
+                this.mensal.vazao = data.vazao;
             })
         },
         async asyncData() {
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const server = http.createServer(app);
 const io = require('socket.io')(server)
 const port = process.env.PORT || 3000
 const isProd = process.env.NODE_ENV === 'production'
+const axios = require('axios');
 
 const api = require('./api');
 
@@ -25,28 +26,59 @@ app.use(nuxt.render);
 server.listen(port, '127.0.0.1')
 console.log('Server listening on localhost:' + port);
 
-// Socket.io
-const options = {
-  host: 'www.google.com',
-  port: 80,
-  path: '/upload',
-  method: 'POST'
-};
+io.on('connection',  (socket) => {
+  let vazaoAtual = 0;
+  let valorConsumoAtual = 0;
+  let vazaoSemanal = 0;
+  let valorConsumoSemanal = 0;
+  let vazaoMensal = 0;
+  let valorConsumoMensal = 0;
 
-const req = http.request(options, function(res) {
-  console.log('STATUS: ' + res.statusCode);
-  res.setEncoding('utf8');
-  res.on('data', function (chunk) {
-    console.log('BODY: ' + chunk);
-  });
-});
-
-req.on('error', function(e) {
-  console.log('problem with request: ' + e.message);
-});
-
-io.on('connection', (socket) => {
   setInterval(() => {
-    socket.broadcast.emit('teste', {preco: (Math.random() * 100).toFixed(2), vazao: (Math.random() * 10).toFixed(2)})
-  }, 1000)
+    axios.post('http://10.0.0.50/AppHydro/qtdLitros.php').then(data => {
+      if (data.data != 0) {
+        socket.broadcast.emit('atual', { preco: valorConsumoAtual, vazao: data.data })
+        vazaoAtual = data.data;
+      }
+    });
+
+    axios.post('http://10.0.0.50/AppHydro/valorConsumo.php').then(data => {
+      if(data.data != 0){
+        socket.broadcast.emit('atual', { preco: data.data, vazao: vazaoAtual })
+        valorConsumoAtual = data.data;
+      }
+    });
+
+    //semanal
+    axios.post('http://10.0.0.50/AppHydro/qtdSemanal.php').then(data => {
+      if (data.data != 0) {
+        socket.broadcast.emit('semanal', { preco: valorConsumoSemanal, vazao: data.data })
+        vazaoSemanal = data.data;
+      }
+    });
+
+    axios.post('http://10.0.0.50/AppHydro/valorSemanal.php').then(data => {
+      if (data.data != 0) {
+        socket.broadcast.emit('semanal', { preco: data.data, vazao: vazaoSemanal })
+        valorConsumo = data.data;
+      }
+    });
+
+       //mensal
+
+    axios.post('http://10.0.0.50/AppHydro/qtdMensal.php').then(data => {
+      if (data.data != 0) {
+        socket.broadcast.emit('mensal', { preco: valorConsumoMensal, vazao: data.data })
+        vazaoMensal = data.data;
+      }
+    });
+
+    axios.post('http://10.0.0.50/AppHydro/valorMensal.php').then(data => {
+      if (data.data != 0) {
+        socket.broadcast.emit('mensal', { preco: data.data, vazao: vazaoMensal })
+        valorConsumoMensal = data.data;
+      }
+    });
+    
+  }, 2500)
 });
